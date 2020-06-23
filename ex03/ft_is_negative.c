59aa50425cab4e833f546067e6e8ee1cbe1ef69a
Nthabiseng Mokgawa <nmokgawa@c4r9s10.wethinkcode.co.za>
2020-06-23 12:01:32 +0200

    day02

--- ex03/ft_is_negative.c
+++ ex03/ft_is_negative.c
@@ -1,10 +1,35 @@
+<<<<<<< HEAD
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmokgawa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/23 11:40:30 by nmokgawa          #+#    #+#             */
+/*   Updated: 2020/06/23 11:41:44 by nmokgawa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+=======
 #include <unistd.h>
 int ft_putchar(char c)
+>>>>>>> 9a34bb936d395081f136b420cc54a9b8b6cc779c
 
-void ft_is_negative(int n);
+#include <unistd.h>
+
+int ft_putchar(char c)
+{ 	write(1,&c,1);
+	return 0;
+
+}
+	void ft_is_negative(int n);
 
 {
+<<<<<<< HEAD
+	if (n > '0'); 
+=======
 	if (n > '0') 
+>>>>>>> 9a34bb936d395081f136b420cc54a9b8b6cc779c
 	ft_putchar('N');
 	else 
 		ft_putchar('P');
--- ex04/ft_print_comb.c
+++ ex04/ft_print_comb.c
@@ -1,10 +1,43 @@
-#include <stdio.h>
+#include <unistd.h>
 
-void ft_print_comb(void);
+void ft_putchar(char c)
+{
+	write(1,&c,1);
+}
+
+void ft_print_comb(void)
 
 { 
-	char dig1
-	char dig2
-	char dig3
+	char a;
+	char b;
+	char c;
 
+	a='0';
+
+	while (a <='7')
+	{ 
+		b = '1';
+
+		while ( b <='8')
+		{
+			c = '2';
+
+			while (c <='9')
+			{
+				if(a != b && a != c && b != c)
+				{
+					ft_putchar(a);
+					ft_putchar(b);
+					ft_putchar(c);
+					ft_putchar(',');
+					ft_putchar(' ');
+				}
+				c = c + 1;
+			}
+			b = b + 1;
+		}
+		a = a + 1;
+	}
+
+}
 
